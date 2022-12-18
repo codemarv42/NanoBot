51b94e84ef473fb69c3a4e7606429e0a9ce3b8b7
Marvin Ramm <88335544+codemarv42@users.noreply.github.com>
2022-12-18 11:06:17 +0100

    Delete Software/NanoOS_V0.35 directory

--- Software/NanoOS_V0.35/ESP32_WiFi.h
+++
@@ -1,8 +0,0 @@
-#ifndef ESP32_WIFI_H
-#define ESP32_WIFI_H
-
-//extern void WiFiSetup();
-
-//extern int WiFiMode;
-
-#endif
--- Software/NanoOS_V0.35/Tests.h
+++
@@ -1,7 +0,0 @@
-#ifndef TESTS_H
-#define TESTS_H
-
-extern void MotorTest();
-extern void LightTest();
-
-#endif
