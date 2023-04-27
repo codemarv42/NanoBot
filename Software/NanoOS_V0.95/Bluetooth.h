872704fc9dadc54fe456731ae6ce17ada6595926
Marvin Ramm <88335544+codemarv42@users.noreply.github.com>
2023-04-27 09:28:01 +0200

    Add files via upload

---
+++ Software/NanoOS_V0.95/Bluetooth.h
@@ -0,0 +1,17 @@
+#ifndef BLUETOOTH_H
+#define BLUETOOTH_H
+/*
+extern int BTSpeed;
+extern String BTNameModel;
+extern String BTName;
+extern bool BTLog;
+extern float BTVersion;
+
+extern void BTOn();
+extern void BTOff();
+extern void BTPrintData();
+extern int GenerateRandomNumber(int digits);
+extern bool BTConnected();
+
+*/
+#endif
